83b095dd0aac11d965e07f4bba8fbd818446b70f
zarazebra <sarah.iber.90@gmail.com>
2024-01-28 12:53:16 +0100

    Add courseservice class and courserepository class

---
+++ courserepository.py
@@ -0,0 +1,30 @@
+from base import Course
+from database import DataBase
+from sqlalchemy import select
+
+
+class CourseRepository:
+    def __init__(self):
+        self.session = DataBase().get_session()
+
+    def add_course(self, professor_id, subject_id):
+        new_course = Course(professor_id=professor_id, subject_id=subject_id)
+        self.session.add(new_course)
+        self.session.commit()
+
+    def edit_course(self, course_id, new_professor_id, new_subject_id):
+        course = self.session.scalar(select(Course).where(Course.id == course_id))
+        if new_professor_id:
+            course.professor_id = new_professor_id
+        if new_subject_id:
+            course.subject_id = new_subject_id
+        self.session.commit()
+
+    def delete_course(self, course_id):
+        course = self.session.scalar(select(Course).where(Course.id == course_id))
+        self.session.delete(course)
+        self.session.commit()
+
+    def get_all_courses(self):
+        all_courses = self.session.scalars(select(Course)).all()
+        return all_courses
---
+++ courseservice.py
@@ -0,0 +1,64 @@
+from display import Display
+from courserepository import CourseRepository
+
+
+class CourseService:
+    def __init__(self):
+        self.course_repository = CourseRepository()
+        self.display = Display()
+
+    def showing_all_courses(self):
+        self.display.clear()
+        all_courses = self.course_repository.get_all_courses()
+        course_info = [(info.id, info.professor_id, info.subject_id) for info in all_courses]
+        self.display.print_table(
+            table_title="List of all courses",
+            column_titles=["ID", "Professor ID", "Subject ID"],
+            row_values=course_info
+        )
+        next_step = input("Do you want to return to the course menu? (y): ").lower()
+        return next_step
+
+    def adding_course(self):
+        self.display.clear()
+        print("Please add the details of the new course.")
+        professor_id = input("Professor ID: ")
+        subject_id = input("Subject ID: ")
+        self.course_repository.add_course(professor_id, subject_id)
+        self.display.clear()
+        print(f"Successfully added new course.")
+        next_step = input("Do you want to return to the course menu, press '1'.\n"
+                          "If you want to add another course, press '2'.\n->  ")
+        return next_step
+
+    def editing_course(self):
+        self.display.clear()
+        print("Please provide the course ID of the course you want to edit and the new details.")
+        course_id = int(input("course ID: "))
+        edit_prof = input("Do you want to edit the professor ID? (y/n): ").lower()
+        if edit_prof == "y":
+            new_professor_id = input("Professor ID: ")
+        else:
+            new_professor_id = None
+        edit_subj = input("Do you want to edit the subject ID? (y/n): ").lower()
+        if edit_subj == "y":
+            new_subject_id = input("Subject ID: ")
+        else:
+            new_subject_id = None
+        self.course_repository.edit_course(course_id, new_professor_id, new_subject_id)
+        self.display.clear()
+        print(f"Successfully changed course with ID '{course_id}'.")
+        next_step = input("Do you want to return to the course menu, press '1'.\n"
+                          "If you want to edit another course, press '2'.\n->  ")
+        return next_step
+
+    def deleting_course(self):
+        self.display.clear()
+        print("Please provide the course ID of the course you want to delete from the database.")
+        course_id = int(input("course ID: "))
+        self.course_repository.delete_course(course_id)
+        self.display.clear()
+        print(f"Successfully deleted the course from the database.")
+        next_step = input("Do you want to return to the course menu, press '1'.\n"
+                          "If you want to delete another course from the database, press '2'.\n->  ")
+        return next_step
